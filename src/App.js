8cb407d5b1b9d46590e89a59b74c8408871edcda
pineapplentt <jvsalss@gmail.com>
2023-08-28 21:44:23 -0300

    new sidebar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MiniDrawer from './teste';
+import CustomizedHook from './search';
+
+function App() {
+  return ( 
+    <>
+      <MiniDrawer />
+    </>
+  );
+}
+
+export default App;
