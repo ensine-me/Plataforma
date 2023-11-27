ff27f167a06ea06fb786990f8d77f1bc9301a661
PedroHFD13 <pedraohfd11@gmail.com>
2023-11-27 18:09:43 -0300

    teste

--- src/pages/PaginaInicialInstitucional.jsx
+++ src/pages/PaginaInicialInstitucional.jsx
@@ -14,7 +14,7 @@ const PaginaInicial = () => {
     return (
         <>
             <Menu />
-            <FirstSection text={'Educação de qualidade a todos'} />
+            <FirstSection text={'Ai calica, funcionou'} />
             <QuemSomosSection />
             <ComoFuncionaSection />
             <OferecemosSection />
